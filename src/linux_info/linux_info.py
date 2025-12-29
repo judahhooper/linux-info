0aaf46882bc2eeaca48e230b12d560ba2d2d7487
judahhooper <judahhooper@icloud.com>
2025-12-29 13:57:31 +0100

    add some commenting

--- src/linux_info/linux_info.py
+++ src/linux_info/linux_info.py
@@ -6,10 +6,14 @@ import paramiko, csv
 # Flags can be used to specify the level of detail. Where information is not available, a specific exit code is returned. 
 
 def linux_info():
+
     host = '100.121.243.99'
     user = 'judah'
     
+
     client = paramiko.SSHClient()
+
+    # so no environment variables are ever required, downside is that this is not portable
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(
@@ -17,12 +21,12 @@ def linux_info():
         username=user, 
         port='2222',)
     
-    stdin_, stdout_, stderr_ = client.exec_command('''ls -l ~                                         
+    stdin_, stdout_, stderr_ = client.exec_command('''                                        
                                                    hostname
                                                    ifconfig -a
                                                    systemctl list-units --all
                                                    ps aux
-                                                   atopsar -c''')
+                                                   free -h''')
     stdout_.channel.recv_exit_status()
     lines = stdout_.readlines()
     
