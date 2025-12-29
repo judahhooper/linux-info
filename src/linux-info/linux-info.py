bb7fe1a97cacd8d4ed9b454e6cde57f085988ad6
judahhooper <judahhooper@icloud.com>
2025-12-29 12:25:16 +0100

    configure simple ssh connection

---
+++ src/linux-info/linux-info.py
@@ -0,0 +1,29 @@
+import paramiko, platform
+
+
+# the script will access a host via ssh and retrieve useful information. 
+
+# Flags can be used to specify the level of detail. Where information is not available, a specific exit code is returned. 
+
+def connect_over_keys():
+    host = '100.121.243.99'
+    user = 'judah'
+    
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(
+        host, 
+        username=user, 
+        port='2222',)
+    
+    stdin_, stdout_, stderr_ = client.exec_command("ls -l ~")
+    stdout_.channel.recv_exit_status()
+    lines = stdout_.readlines()
+    
+    for line in lines:
+        print(line)
+
+    client.close()
+
+connect_over_keys()
